8fb5263ef99238f0482fbb7706da4586c8bea89a
richiecabantog22 <richiepogi22@gmail.com>
2025-12-12 00:13:06 +0800

    test

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-adminJs.watch();
-adminJs.bundle("./admin-dashboard.jsx", "admin-dashboard.jsx");
+
 
 
 // --------------------------
